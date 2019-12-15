c13bb673682c760b0ac743f8b07ab7493e6b06c9
Brent <mina512@live.com>
2019-12-15 15:59:44 -0600

    Updated controller with all routes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const burger = require('../models/burger');
+
+router.get('/', (req, res) => {
+    burger.all(data => res.render('index', {burger: data}));
+});
+
+router.post('/api/burgers', (req, res) => {
+    burger.create(req.body.burger_name, result => {
+        console.log(result);
+        res.json(result);
+    });
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+    burger.update(req.params.id, result => {
+        console.log(result);
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router;
